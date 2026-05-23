a1350dfc246e1a95511211f270d87c35d43623f1
szejing <szejing.go@gmail.com>
2026-05-23 10:18:05 +0800

    feat: extend MerchantCustomerMetadata with additional order tracking fields

--- dist/cjs/src/metadata/merchant-customer.d.ts
+++ dist/cjs/src/metadata/merchant-customer.d.ts
@@ -1,4 +1,8 @@
 export type MerchantCustomerMetadata = {
     total_spent: number;
+    pending_orders: number;
+    cancelled_orders: number;
+    refunded_orders: number;
+    completed_orders: number;
 };
 //# sourceMappingURL=merchant-customer.d.ts.map
--- src/metadata/merchant-customer.ts
+++ src/metadata/merchant-customer.ts
@@ -1,3 +1,7 @@
 export type MerchantCustomerMetadata = {
 	total_spent: number;
+	pending_orders: number;
+	cancelled_orders: number;
+	refunded_orders: number;
+	completed_orders: number;
 };
